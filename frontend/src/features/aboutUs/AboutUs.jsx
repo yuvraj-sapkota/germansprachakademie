5a7414ffc31e5ceaf249c9c1d572f016a64f981b
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2026-06-02 06:53:45 +0545

    worked on image on mobile screen of about page

--- frontend/src/features/aboutUs/AboutUs.jsx
+++ frontend/src/features/aboutUs/AboutUs.jsx
@@ -221,7 +221,7 @@ export default function About() {
             transition={{ duration: 0.7 }}
           >
             <img
-              src="https://plus.unsplash.com/premium_photo-1671070687988-b2d6805ea896?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src={about}
               alt="Students learning German at German Sprach Akademie"
               className="w-full h-full object-cover object-center"
             />
